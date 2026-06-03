b83427bbb01f9106ef3466d8f4fbf58322d22093
CaioRenatoDot <caiorenato146@gmail.com>
2026-06-03 15:02:17 -0300

    feat:SMOOTH SCROLLING ADICIONADO EBAA

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import Lenis from "lenis";
 import "./styles.css";
 import App from "./App.jsx";
+
+const lenis = new Lenis({
+  duration: 1.2,
+  smoothWheel: true,
+  smoothTouch: false,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
